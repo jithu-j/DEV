51d7b8c4aa2904090c0a890527abe9a809f915f4
jithu-j <jithujkumar1010@gmail.com>
2021-05-08 21:12:08 +0530

    Conditional Rendering

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -3,12 +3,70 @@
 console.log('App.js is working!');
 
 //JSX - Javascript XML
+
+function getSubtitle(sb) {
+    if (sb) {
+        return React.createElement(
+            'p',
+            null,
+            main.subtitle
+        );
+    }
+}
+
+var main = {
+    title: 'Indecision App',
+    subtitle: 'First para',
+    options: ['one', 'two']
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        main.title && main.title
+    ),
+    getSubtitle(main.subtitle),
+    main.options.length > 0 ? 'Available options' : 'No Options',
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Mango'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Orange'
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Jithu'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age:24'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location:India'
+    )
 );
 
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+//ReactDOM.render(templateTwo,appRoot);
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,8 +1,40 @@
 console.log('App.js is working!');
 
 //JSX - Javascript XML
-var template = <h1>Indecision App</h1>;
+
+function getSubtitle (sb) {
+    if (sb) {
+        return <p>{main.subtitle}</p>;
+    }
+}
+
+var main = {
+    title: 'Indecision App',
+    subtitle: 'First para',
+    options: ['one','two']
+};
+
+var template = (
+    <div>
+        <h1>{main.title && main.title}</h1>
+        {getSubtitle(main.subtitle)}
+        {main.options.length>0 ? 'Available options' : 'No Options'}
+        <ol>
+            <li>Mango</li>
+            <li>Orange</li>
+        </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Jithu</h1>
+        <p>Age:24</p>
+        <p>Location:India</p>
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template,appRoot);
+//ReactDOM.render(templateTwo,appRoot);
