044f7a3e4564e13c7eee8c5b13e10cae5250c6c1
jithu-j <jithujkumar1010@gmail.com>
2023-04-26 12:17:31 +0530

    Learning Spring Boot Push 2

--- Spring Boot/src/main/java/com/perproj/learnspringboot/CourseController.java
+++ Spring Boot/src/main/java/com/perproj/learnspringboot/CourseController.java
@@ -22,7 +22,8 @@ public class CourseController {
     public List<Course> retrieveAllCourses(){
         return Arrays.asList(
                 new Course(1, "Learn AWS" , "Jack"),
-                new Course(2, "Learn DevOps", "Jill")
+                new Course(2, "Learn DevOps", "Jill"),
+                new Course(2, "Learn Azure", "John")
         );
     }
 }
