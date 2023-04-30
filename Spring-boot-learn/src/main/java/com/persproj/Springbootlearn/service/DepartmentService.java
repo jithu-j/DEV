df2f10804565c85ba3a77a7042d160e526622342
jithu-j <jithujkumar1010@gmail.com>
2023-04-30 14:02:56 +0530

    Spring Boot Learning push 5

---
+++ Spring-boot-learn/src/main/java/com/persproj/Springbootlearn/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package com.persproj.Springbootlearn.service;
+
+import com.persproj.Springbootlearn.entity.Department;
+import com.persproj.Springbootlearn.error.DepartmentNotFoundException;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
+}
